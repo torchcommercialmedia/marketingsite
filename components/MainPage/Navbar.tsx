363538e0bb576fb09266908b945cd70b5e8c960b
Kelxety <llanzanarobinjohn@gmail.com>
2023-06-03 22:52:11 +0800

    added navbar and navigation component

---
+++ components/MainPage/Navbar.tsx
@@ -0,0 +1,154 @@
+"use client";
+
+import React, { useMemo, useState } from "react";
+import { usePathname } from "next/navigation";
+import { Dialog } from "@headlessui/react";
+import Image from "next/image";
+import { HiBars3, HiXMark } from "react-icons/hi2";
+import Navigation from "./Navigation";
+
+type NavbarProps = {
+  children: React.ReactNode;
+};
+
+const Navbar: React.FC<NavbarProps> = ({ children }) => {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+  const pathname = usePathname();
+
+  const routes = useMemo(
+    () => [
+      {
+        label: "Product",
+        active: pathname === "/search",
+        href: "/product",
+      },
+      {
+        label: "Uses",
+        active: pathname === "/search",
+        href: "/uses",
+      },
+      {
+        label: "Pricing",
+        active: pathname === "/search",
+        href: "/pricing",
+      },
+      {
+        label: "About Us",
+        active: pathname === "/search",
+        href: "/aboutus",
+      },
+    ],
+    [pathname]
+  );
+  return (
+    <nav
+      className="flex items-center justify-between p-6 lg:px-8"
+      aria-label="Global"
+    >
+      <div className="flex lg:flex-1">
+        <a href="#" className="-m-1.5 p-1.5">
+          <span className="sr-only">Chromelot</span>
+          <Image
+            src={"/logo/chromelotlogo.png"}
+            className="h-8 w-auto"
+            width={100}
+            height={100}
+            alt={""}
+          />
+        </a>
+      </div>
+      <div className="flex lg:hidden">
+        <button
+          type="button"
+          className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+          onClick={() => setMobileMenuOpen(true)}
+        >
+          <span className="sr-only">Open main menu</span>
+          <HiBars3 className="h-6 w-6" aria-hidden="true" />
+        </button>
+      </div>
+      <div className="hidden lg:flex lg:gap-x-12">
+        {routes.map((item) => (
+          <Navigation
+            label={item.label}
+            active={item.active}
+            href={item.href}
+            key={item.label}
+          >
+            {children}
+          </Navigation>
+        ))}
+      </div>
+      <div className="hidden lg:flex lg:flex-1 lg:justify-end space-x-2">
+        <a
+          href="#"
+          className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+        >
+          Request a Demo
+        </a>
+        <a
+          href="#"
+          className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+        >
+          Sign Up
+        </a>
+      </div>
+      <Dialog
+        as="div"
+        className="lg:hidden"
+        open={mobileMenuOpen}
+        onClose={setMobileMenuOpen}
+      >
+        <div className="fixed inset-0 z-50" />
+        <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+          <div className="flex items-center justify-between">
+            <a href="#" className="-m-1.5 p-1.5">
+              <span className="sr-only">Your Company</span>
+              <Image
+                src={"/logo/chromelotlogo.png"}
+                className="h-8 w-auto"
+                width={100}
+                height={100}
+                alt={""}
+              />
+            </a>
+            <button
+              type="button"
+              className="-m-2.5 rounded-md p-2.5 text-gray-700"
+              onClick={() => setMobileMenuOpen(false)}
+            >
+              <span className="sr-only">Close menu</span>
+              <HiXMark className="h-6 w-6" aria-hidden="true" />
+            </button>
+          </div>
+          <div className="mt-6 flow-root">
+            <div className="-my-6 divide-y divide-gray-500/10">
+              <div className="space-y-2 py-6">
+                {routes.map((item) => (
+                  <Navigation
+                    label={item.label}
+                    active={item.active}
+                    href={item.href}
+                    key={item.label}
+                  >
+                    {children}
+                  </Navigation>
+                ))}
+              </div>
+              <div className="py-6">
+                <a
+                  href="#"
+                  className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                >
+                  Log in
+                </a>
+              </div>
+            </div>
+          </div>
+        </Dialog.Panel>
+      </Dialog>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ components/MainPage/Navigation.tsx
@@ -0,0 +1,125 @@
+"use client";
+
+import { Popover, Transition } from "@headlessui/react";
+import React, { Fragment, useState } from "react";
+import { AiOutlinePaperClip } from "react-icons/ai";
+import {
+  MdWrongLocation,
+  MdOutlinePayments,
+  MdOutlineInventory,
+} from "react-icons/md";
+import { BsArrowLeftRight, BsTools } from "react-icons/bs";
+import { GiAutoRepair } from "react-icons/gi";
+import { RiStoreFill } from "react-icons/ri";
+import { IconType } from "react-icons";
+
+type NavigationProps = {
+  children: React.ReactNode;
+  label: string;
+  active: boolean;
+  href: string;
+};
+
+type DivArray = {
+  icon: IconType;
+  size: number;
+  text: string;
+};
+
+type PopoverContent = {
+  [key: string]: React.ReactNode;
+};
+
+const Navigation: React.FC<NavigationProps> = ({
+  children,
+  label,
+  active,
+  href,
+}) => {
+  const ProductArray: DivArray[] = [
+    { icon: AiOutlinePaperClip, size: 24, text: "Repair Orders" },
+    { icon: GiAutoRepair, size: 24, text: "2 Way SMS" },
+    { icon: MdWrongLocation, size: 24, text: "Mobile Dispatch" },
+    { icon: BsArrowLeftRight, size: 22, text: "Vendor/Dealer Integration" },
+    { icon: BsTools, size: 22, text: "Reconditioning Management" },
+    { icon: MdOutlinePayments, size: 24, text: "Invoice & Payments" },
+    { icon: RiStoreFill, size: 22, text: "Merchandising Tools" },
+    { icon: MdOutlineInventory, size: 24, text: "Inventory Management" },
+    { icon: BsTools, size: 24, text: "Parts Procurement" },
+    { icon: MdOutlinePayments, size: 24, text: "Dashboard & Reporting" },
+  ];
+
+  const UsesProduct: DivArray[] = [
+    { icon: AiOutlinePaperClip, size: 24, text: "Classic & Exotic Dealers" },
+    { icon: AiOutlinePaperClip, size: 24, text: "Mobile Repair Shops" },
+    { icon: AiOutlinePaperClip, size: 24, text: "Wheel Refinishing" },
+  ];
+
+  const rows = [];
+  for (let i = 0; i < ProductArray.length; i += 2) {
+    const div1 = ProductArray[i];
+    const div2 = ProductArray[i + 1];
+    rows.push(
+      <div key={i} className="flex">
+        <div className="relative flex-1 rounded-lg p-4 hover:bg-gray-50 flex space-x-4">
+          <div1.icon size={div1.size} />
+          <div className="text-gray-900">
+            {div1.text}
+            <span className="absolute inset-0" />
+          </div>
+        </div>
+        {div2 && (
+          <div className="relative flex-1 rounded-lg p-4 hover:bg-gray-50 flex space-x-4">
+            <div2.icon size={div2.size} />
+            <div className="text-gray-900">
+              {div2.text}
+              <span className="absolute inset-0" />
+            </div>
+          </div>
+        )}
+      </div>
+    );
+  }
+
+  const popoverContent: PopoverContent = {
+    product: (
+      <div className="w-screen max-w-sm flex-auto rounded-3xl bg-white p-4 text-sm leading-6 shadow-lg ring-1 ring-gray-900/5 justify-between lg:max-w-lg">
+        {rows}
+      </div>
+    ),
+    uses: <div className="flex">{/* Content for Uses */}</div>,
+    pricing: <div className="flex">{/* Content for Pricing */}</div>,
+    aboutus: <div className="flex">{/* Content for About Us */}</div>,
+  };
+
+  const popoverContentToShow = popoverContent[href.replace("/", "")];
+
+  return (
+    <Popover
+      key={label}
+      // href={href}
+      // className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+      className="relative"
+    >
+      <Popover.Button className="inline-flex items-center gap-x-1 text-sm font-semibold leading-6 text-gray-900 outline-none">
+        <span>{label}</span>
+      </Popover.Button>
+
+      <Transition
+        as={Fragment}
+        enter="transition ease-out duration-200"
+        enterFrom="opacity-0 translate-y-1"
+        enterTo="opacity-100 translate-y-0"
+        leave="transition ease-in duration-150"
+        leaveFrom="opacity-100 translate-y-0"
+        leaveTo="opacity-0 translate-y-1"
+      >
+        <Popover.Panel className="absolute left-1/2 z-10 mt-5 flex w-screen max-w-max -translate-x-1/2 px-4">
+          <div className="flex">{popoverContentToShow}</div>
+        </Popover.Panel>
+      </Transition>
+    </Popover>
+  );
+};
+
+export default Navigation;
