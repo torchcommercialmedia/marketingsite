186aacefcad29905b260ff6a53a2b224fe1cbac8
Kelxety <llanzanarobinjohn@gmail.com>
2023-06-03 22:36:20 +0800

    added navbar component

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
+import Navbar from "@/components/Navbar";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen ">
       {/* Hi */}
-      Hi
+      <Navbar />
     </main>
   );
 }
