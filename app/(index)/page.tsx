db1614c2337b9c9e5dd7d58e3a5c189789f90c7a
Kelxety <llanzanarobinjohn@gmail.com>
2023-06-05 03:20:17 +0800

    remove types

--- app/(index)/page.tsx
+++ app/(index)/page.tsx
@@ -1,14 +1,9 @@
 import Hero from "@/components/MainPage/Hero";
 import Tools from "@/components/MainPage/Tools";
 
-interface IndexProp {
-  children: React.ReactNode;
-}
-
-export default function Home({ children }: IndexProp) {
+export default function Home() {
   return (
     <main className="flex w-full flex-col">
-      {children}
       <Hero />
       <Tools />
     </main>
