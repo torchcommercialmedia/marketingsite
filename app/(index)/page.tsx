d1b0d34797ad3caee97eaf120d43b8a3792fcb22
Kelxety <llanzanarobinjohn@gmail.com>
2023-06-07 00:56:34 +0800

    added service compony and exampluses

--- app/(index)/page.tsx
+++ app/(index)/page.tsx
@@ -1,5 +1,7 @@
 import DealersAndServiceProvider from "@/components/MainPage/DealersAndServiceProvider";
+import ExampleUses from "@/components/MainPage/ExampleUses";
 import Hero from "@/components/MainPage/Hero";
+import ServiceCompany from "@/components/MainPage/ServiceCompany";
 import Tools from "@/components/MainPage/Tools";
 import ToolsForDealers from "@/components/MainPage/ToolsForDealers";
 
@@ -10,6 +12,8 @@ export default function Home() {
       <Tools />
       <DealersAndServiceProvider />
       <ToolsForDealers />
+      <ServiceCompany />
+      <ExampleUses />
     </main>
   );
 }
