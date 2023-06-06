69303efce1f2ff6c65dc63229cd52870f40abdbe
Kelxety <llanzanarobinjohn@gmail.com>
2023-06-06 20:51:37 +0800

    Added dealers and service provider

--- app/(index)/page.tsx
+++ app/(index)/page.tsx
@@ -1,11 +1,15 @@
+import DealersAndServiceProvider from "@/components/MainPage/DealersAndServiceProvider";
 import Hero from "@/components/MainPage/Hero";
 import Tools from "@/components/MainPage/Tools";
+import ToolsForDealers from "@/components/MainPage/ToolsForDealers";
 
 export default function Home() {
   return (
     <main className="flex w-full flex-col">
       <Hero />
       <Tools />
+      <DealersAndServiceProvider />
+      <ToolsForDealers />
     </main>
   );
 }
