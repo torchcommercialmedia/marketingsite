8cd778430209dccbcf19ca8d387d407e06311780
Kelxety <llanzanarobinjohn@gmail.com>
2023-06-08 20:50:59 +0800

    Fix UI

--- components/MainPage/BuiltWithAi.tsx
+++ components/MainPage/BuiltWithAi.tsx
@@ -4,27 +4,34 @@ type Props = {};
 
 const BuiltWithAi = (props: Props) => {
   return (
-    <section className="mx-auto max-w-7xl min-h-screen flex items-center flex-col">
+    <section className="mx-auto max-w-7xl min-h-screen flex items-center flex-col mt-24 sm:mt-0">
       <h2 className="font-bold text-3xl">Built with AI</h2>
       <h3>AI Tools to accelerate your workflow</h3>
-      <div className="p-8"></div>
-      <div className="mx-auto grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-12 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 sm:gap-y-14 lg:mx-0 lg:max-w-none lg:grid-cols-5"></div>
-      <div className="p-4"></div>
-      <div className="w-full flex space-x-8">
-        <div className="h-96 w-72 border rounded-xl">
-          <h3 className="font-block font-semibold text-center text-xl mt-4">
+      <div className="sm:p-8"></div>
+      <div className="w-full flex flex-col sm:flex-row space-y-4 sm:space-y-0 sm:space-x-8">
+        <div className="w-72 rounded-xl">
+          <h3 className="font-block h-16 font-semibold text-center text-lg mt-4">
             AI Photo Background Replacement
           </h3>
+          <div className="h-full w-full border pt-24 pb-24 mx-auto">
+            <p className="text-center">Image or Content Here</p>
+          </div>
         </div>
-        <div className="h-96 w-72 border rounded-xl">
-          <h3 className="font-block font-semibold text-center text-xl mt-4">
+        <div className="w-72 rounded-xl">
+          <h3 className="font-block h-16 font-semibold text-center text-lg mt-4">
             GPT Assisted Description Writer
           </h3>
+          <div className="h-full w-full border pt-24 pb-24 mx-auto">
+            <p className="text-center">Image or Content Here</p>
+          </div>
         </div>
-        <div className="h-96 w-72 border rounded-xl">
-          <h3 className="font-block font-semibold text-center text-xl mt-4">
+        <div className="w-72 rounded-xl">
+          <h3 className="font-block h-16 font-semibold text-center text-lg mt-4">
             AI Augmented Photo Editor
           </h3>
+          <div className="h-full w-full border pt-24 pb-24 mx-auto">
+            <p className="text-center">Image or Content Here</p>
+          </div>
         </div>
       </div>
     </section>
