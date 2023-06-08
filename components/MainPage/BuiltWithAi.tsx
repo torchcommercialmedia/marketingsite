9c77e4c27cc41cf77214b98ae4533d1805f90556
Kelxety <llanzanarobinjohn@gmail.com>
2023-06-08 21:08:43 +0800

    fix layout by grid

--- components/MainPage/BuiltWithAi.tsx
+++ components/MainPage/BuiltWithAi.tsx
@@ -4,12 +4,12 @@ type Props = {};
 
 const BuiltWithAi = (props: Props) => {
   return (
-    <section className="mx-auto max-w-7xl min-h-screen flex items-center flex-col mt-24 sm:mt-0">
+    <section className="mx-auto max-w-7xl min-h-screen flex h-auto items-center flex-col pt-24 pb-24 sm:mt-0">
       <h2 className="font-bold text-3xl">Built with AI</h2>
       <h3>AI Tools to accelerate your workflow</h3>
       <div className="sm:p-8"></div>
-      <div className="w-full flex flex-col sm:flex-row space-y-4 sm:space-y-0 sm:space-x-8">
-        <div className="w-72 rounded-xl">
+      <div className="w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+        <div className="col-span-1">
           <h3 className="font-block h-16 font-semibold text-center text-lg mt-4">
             AI Photo Background Replacement
           </h3>
@@ -17,7 +17,7 @@ const BuiltWithAi = (props: Props) => {
             <p className="text-center">Image or Content Here</p>
           </div>
         </div>
-        <div className="w-72 rounded-xl">
+        <div className="col-span-1">
           <h3 className="font-block h-16 font-semibold text-center text-lg mt-4">
             GPT Assisted Description Writer
           </h3>
@@ -25,7 +25,7 @@ const BuiltWithAi = (props: Props) => {
             <p className="text-center">Image or Content Here</p>
           </div>
         </div>
-        <div className="w-72 rounded-xl">
+        <div className="col-span-1">
           <h3 className="font-block h-16 font-semibold text-center text-lg mt-4">
             AI Augmented Photo Editor
           </h3>
