f9e6101e83f3c33af9f8e611ad63528d6c142f64
Kelxety <llanzanarobinjohn@gmail.com>
2023-06-11 19:05:26 +0800

    Added team components

--- app/about-us/page.tsx
+++ app/about-us/page.tsx
@@ -1,4 +1,7 @@
 import HeroAbout from "@/components/AboutUs/HeroAbout";
+import SinceThen from "@/components/AboutUs/SinceThen";
+import TheTeam from "@/components/AboutUs/TheTeam";
+import Footer from "@/components/MainPage/Footer";
 import React from "react";
 
 type Props = {};
@@ -7,6 +10,9 @@ const page = (props: Props) => {
   return (
     <div className="flex w-full flex-col">
       <HeroAbout />
+      <SinceThen />
+      <TheTeam />
+      <Footer />
     </div>
   );
 };
