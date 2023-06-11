203a0f1db8ba7444b491da47d4b41ba633261a23
Kelxety <llanzanarobinjohn@gmail.com>
2023-06-11 19:05:40 +0800

    added components

---
+++ components/AboutUs/SinceThen.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+type Props = {};
+
+const SinceThen = (props: Props) => {
+  return (
+    <div className="w-full p-24 max-w-7xl mx-auto min-h-screen h-full">
+      SinceThen
+    </div>
+  );
+};
+
+export default SinceThen;
