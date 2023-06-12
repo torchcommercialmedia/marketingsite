205e0c1bd3582aadfdf8f8b8c2a4b25bb403a325
Kelxety <llanzanarobinjohn@gmail.com>
2023-06-12 15:00:12 +0800

    added table components for more features

--- app/pricing/page.tsx
+++ app/pricing/page.tsx
@@ -1,5 +1,7 @@
 "use client";
+import FullFeature from "@/components/Pricing/FullFeature";
 import Toogle from "@/components/Pricing/Toogle";
+import Link from "next/link";
 import React, { useState } from "react";
 import { AiOutlineCheckCircle, AiOutlineCloseCircle } from "react-icons/ai";
 import { BiMessageCheck } from "react-icons/bi";
@@ -13,8 +15,8 @@ import { VscTools } from "react-icons/vsc";
 type Props = {};
 
 const Pricing = (props: Props) => {
-  const [enabled, setEnabled] = useState<boolean>(true);
-
+  const [annualSavingsEnabled, setAnnualSavingsEnabled] =
+    useState<boolean>(true);
   const [showAdditionalFeatures, setShowAdditionalFeatures] =
     useState<boolean>(false);
   const [merchandisingSelected, setMerchandisingSelected] =
@@ -29,12 +31,12 @@ const Pricing = (props: Props) => {
       href: "#",
       priceMonthly:
         merchandisingSelected && serviceToolsSelected
-          ? "$299"
+          ? 299
           : merchandisingSelected && !serviceToolsSelected
-          ? "$249"
+          ? 249
           : !merchandisingSelected && serviceToolsSelected
-          ? "$249"
-          : "$199",
+          ? 249
+          : 199,
       description:
         "Includes 5 users, additional users at " +
         (merchandisingSelected && serviceToolsSelected
@@ -68,46 +70,17 @@ const Pricing = (props: Props) => {
       mostPopular: false,
     },
     {
-      name: "Full ToolKit",
-      id: "full-toolkit",
-      href: "#",
-      priceMonthly:
-        merchandisingSelected && serviceToolsSelected
-          ? "$499"
-          : merchandisingSelected && !serviceToolsSelected
-          ? "$399"
-          : !merchandisingSelected && serviceToolsSelected
-          ? "$399"
-          : "$299",
-      description:
-        "Includes 5 users, additional users at " +
-        (merchandisingSelected && serviceToolsSelected
-          ? "$49"
-          : merchandisingSelected && !serviceToolsSelected
-          ? "$39"
-          : !merchandisingSelected && serviceToolsSelected
-          ? "$39"
-          : "$29") +
-        "/month",
-      initialFeatures: [
-        "All Dealer Features",
-        "All Service Features",
-        "One Integrated Features",
-      ],
-      mostPopular: true,
-    },
-    {
       name: "Service Toolkit",
       id: "service-toolkit",
       href: "#",
       priceMonthly:
         merchandisingSelected && serviceToolsSelected
-          ? "$299"
+          ? 299
           : merchandisingSelected && !serviceToolsSelected
-          ? "$249"
+          ? 249
           : !merchandisingSelected && serviceToolsSelected
-          ? "$249"
-          : "$199",
+          ? 249
+          : 199,
       description:
         "Includes 5 users, additional users at " +
         (merchandisingSelected && serviceToolsSelected
@@ -137,6 +110,35 @@ const Pricing = (props: Props) => {
         "Customizable Dashboard",
         "Full Reporting Features",
         "Multi-Rooftop Support",
+      ],
+      mostPopular: true,
+    },
+    {
+      name: "Full ToolKit",
+      id: "full-toolkit",
+      href: "#",
+      priceMonthly:
+        merchandisingSelected && serviceToolsSelected
+          ? 499
+          : merchandisingSelected && !serviceToolsSelected
+          ? 399
+          : !merchandisingSelected && serviceToolsSelected
+          ? 399
+          : 299,
+      description:
+        "Includes 5 users, additional users at " +
+        (merchandisingSelected && serviceToolsSelected
+          ? "$49"
+          : merchandisingSelected && !serviceToolsSelected
+          ? "$39"
+          : !merchandisingSelected && serviceToolsSelected
+          ? "$39"
+          : "$29") +
+        "/month",
+      initialFeatures: [
+        "All Dealer Features",
+        "All Service Features",
+        "One Integrated Features",
       ],
       mostPopular: false,
     },
@@ -260,78 +262,88 @@ const Pricing = (props: Props) => {
 
             <div className="p-8"></div>
             <div className="p-2 flex space-x-2 justify-center">
-              <Toogle setEnabled={setEnabled} enabled={enabled} />
-              <p>Annual Savings</p>
+              <Toogle
+                setAnnualSavingsEnabled={setAnnualSavingsEnabled}
+                annualSavingsEnabled={annualSavingsEnabled}
+              />
+              {annualSavingsEnabled ? (
+                <p>Annual Savings</p>
+              ) : (
+                <p>Monthly Pricing</p>
+              )}
             </div>
           </div>
-          {enabled && (
-            <div className="isolate mx-auto mt-4 grid max-w-md grid-cols-1 gap-y-8 sm:mt-12 lg:mx-0 lg:max-w-none lg:grid-cols-3">
-              {tiers.map((tier, tierIdx) => (
-                <div
-                  key={tier.id}
-                  className={classNames(
-                    tier.mostPopular ? "lg:z-10 lg:rounded-b-none" : "lg:mt-8",
-                    tierIdx === 0 ? "lg:rounded-r-none" : "",
-                    tierIdx === tiers.length - 1 ? "lg:rounded-l-none" : "",
-                    "flex flex-col justify-between rounded-3xl bg-white p-8 ring-1 ring-gray-200 xl:p-10"
-                  )}
-                >
-                  <div>
-                    <div className="flex items-center justify-between gap-x-4">
-                      <h3
-                        id={tier.id}
-                        className={classNames(
-                          tier.mostPopular ? "text-red-600" : "text-gray-900",
-                          "text-lg font-semibold leading-8"
-                        )}
-                      >
-                        {tier.name}
-                      </h3>
-                      {tier.mostPopular ? (
+          <div className="isolate mx-auto mt-4 grid max-w-md grid-cols-1 gap-y-8 sm:mt-12 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+            {tiers.map((tier, tierIdx) => (
+              <div
+                key={tier.id}
+                className={classNames(
+                  "flex flex-col justify-between rounded-3xl bg-white p-8 ring-1 ring-gray-200 xl:p-10"
+                )}
+              >
+                <div>
+                  <div className="flex items-center justify-between gap-x-4">
+                    <h3
+                      id={tier.id}
+                      className={classNames(
+                        "text-gray-900",
+                        "text-lg font-semibold leading-8"
+                      )}
+                    >
+                      {tier.name}
+                    </h3>
+                    {/* {tier.mostPopular ? (
                         <p className="rounded-full bg-red-600/10 px-2.5 py-1 text-xs font-semibold leading-5 text-red-600">
                           Most popular
                         </p>
-                      ) : null}
-                    </div>
-                    <p className="mt-4 text-sm leading-6 text-gray-600">
-                      {tier.description}
-                    </p>
-                    <p className="mt-6 flex items-baseline gap-x-1">
-                      <span className="text-4xl font-bold tracking-tight text-gray-900">
-                        {tier.priceMonthly}
-                      </span>
-                      <span className="text-sm font-semibold leading-6 text-gray-600">
-                        /month
-                      </span>
-                    </p>
-                    <ul
-                      role="list"
-                      className="mt-8 space-y-3 text-sm leading-6 text-gray-600"
-                    >
-                      {tier?.initialFeatures?.map((feature) => (
-                        <li key={feature} className="flex gap-x-3">
-                          <MdCheckCircleOutline
-                            className="h-6 w-5 flex-none text-red-600"
-                            aria-hidden="true"
-                          />
-                          {feature}
-                        </li>
-                      ))}
-                    </ul>
-                    {tier.additionalFeatures &&
-                      tier.additionalFeatures.length > 0 &&
-                      !showAdditionalFeatures && (
-                        <div className="mt-2 text-center">
-                          <button
-                            className="text-red-600 font-semibold underline"
-                            onClick={() => setShowAdditionalFeatures(true)}
-                          >
-                            View more
-                          </button>
-                        </div>
-                      )}
+                      ) : null} */}
+                  </div>
+                  <p className="mt-4 text-sm leading-6 text-gray-600">
+                    {tier.description}
+                  </p>
+                  <p className="mt-6 flex items-baseline gap-x-1">
+                    <span className="text-4xl font-bold tracking-tight text-gray-900">
+                      $
+                      {annualSavingsEnabled
+                        ? tier.priceMonthly
+                        : (tier.priceMonthly +=
+                            tier.priceMonthly * 0.15).toFixed()}
+                    </span>
+                    <span className="text-sm font-semibold leading-6 text-gray-600">
+                      /month
+                    </span>
+                  </p>
+                  <ul
+                    role="list"
+                    className="mt-8 space-y-3 text-sm leading-6 text-gray-600"
+                  >
+                    {tier?.initialFeatures?.map((feature) => (
+                      <li key={feature} className="flex gap-x-3">
+                        <MdCheckCircleOutline
+                          className="h-6 w-5 flex-none text-red-600"
+                          aria-hidden="true"
+                        />
+                        {feature}
+                      </li>
+                    ))}
+                  </ul>
+                  {tier.additionalFeatures &&
+                    tier.additionalFeatures.length > 0 &&
+                    !showAdditionalFeatures && (
+                      <div className="mt-24 text-center">
+                        <Link
+                          href="/pricing#full-feature"
+                          className="text-red-600 font-semibold underline"
+                          onClick={() => {
+                            setShowAdditionalFeatures(true);
+                          }}
+                        >
+                          View full feature comparison
+                        </Link>
+                      </div>
+                    )}
 
-                    {showAdditionalFeatures && tier.additionalFeatures && (
+                  {/* {showAdditionalFeatures && tier.additionalFeatures && (
                       <>
                         <ul
                           role="list"
@@ -356,26 +368,25 @@ const Pricing = (props: Props) => {
                           </button>
                         </div>
                       </>
-                    )}
-                  </div>
-                  <a
-                    href={tier.href}
-                    aria-describedby={tier.id}
-                    className={classNames(
-                      tier.mostPopular
-                        ? "bg-red-600 text-white shadow-sm hover:bg-red-500"
-                        : "text-red-600 ring-1 ring-inset ring-red-200 hover:ring-red-300",
-                      "mt-8 block rounded-md py-2 px-3 text-center text-sm font-semibold leading-6 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600"
-                    )}
-                  >
-                    Request a Demo
-                  </a>
+                    )} */}
                 </div>
-              ))}
-            </div>
-          )}
+                <a
+                  href={tier.href}
+                  aria-describedby={tier.id}
+                  className={classNames(
+                    tier.mostPopular
+                      ? "bg-red-600 text-white shadow-sm hover:bg-red-500"
+                      : "text-red-600 ring-1 ring-inset ring-red-200 hover:ring-red-300",
+                    "mt-8 block rounded-md py-2 px-3 text-center text-sm font-semibold leading-6 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600"
+                  )}
+                >
+                  Request a Demo
+                </a>
+              </div>
+            ))}
+          </div>
           <div className="p-8"></div>
-          {!enabled && (
+          {!annualSavingsEnabled && (
             <div className="text-center">
               <h3 className="font-semibold">Ready to Get Started?</h3>
               <p>
@@ -389,6 +400,7 @@ const Pricing = (props: Props) => {
             </div>
           )}
         </div>
+        {showAdditionalFeatures && <FullFeature />}
       </div>
     </div>
   );
