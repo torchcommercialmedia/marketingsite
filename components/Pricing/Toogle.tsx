d7d5b7f7d535d4a398b891ef4309af95a428b966
Kelxety <llanzanarobinjohn@gmail.com>
2023-06-12 15:00:58 +0800

    added percentage when toggle is off

--- components/Pricing/Toogle.tsx
+++ components/Pricing/Toogle.tsx
@@ -3,34 +3,34 @@ import { Switch } from "@headlessui/react";
 import React, { useState } from "react";
 
 type Props = {
-  setEnabled: (enabled: boolean) => void;
-  enabled: boolean;
+  setAnnualSavingsEnabled: (annualSavingsEnabled: boolean) => void;
+  annualSavingsEnabled: boolean;
 };
 
 const classNames = (...classes: any[]) => {
   return classes.filter(Boolean).join(" ");
 };
 
-const Toogle = ({ setEnabled, enabled }: Props) => {
+const Toogle = ({ setAnnualSavingsEnabled, annualSavingsEnabled }: Props) => {
   return (
     <Switch
-      checked={enabled}
-      onChange={setEnabled}
+      checked={annualSavingsEnabled}
+      onChange={setAnnualSavingsEnabled}
       className={classNames(
-        enabled ? "bg-red-600" : "bg-gray-200",
+        annualSavingsEnabled ? "bg-red-600" : "bg-gray-200",
         "relative inline-flex h-6 w-11 flex-shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus:ring-2 focus:ring-red-600 focus:ring-offset-2"
       )}
     >
       <span className="sr-only">Use setting</span>
       <span
         className={classNames(
-          enabled ? "translate-x-5" : "translate-x-0",
+          annualSavingsEnabled ? "translate-x-5" : "translate-x-0",
           "pointer-events-none relative inline-block h-5 w-5 transform rounded-full bg-white shadow ring-0 transition duration-200 ease-in-out"
         )}
       >
         <span
           className={classNames(
-            enabled
+            annualSavingsEnabled
               ? "opacity-0 duration-100 ease-out"
               : "opacity-100 duration-200 ease-in",
             "absolute inset-0 flex h-full w-full items-center justify-center transition-opacity"
@@ -53,7 +53,7 @@ const Toogle = ({ setEnabled, enabled }: Props) => {
         </span>
         <span
           className={classNames(
-            enabled
+            annualSavingsEnabled
               ? "opacity-100 duration-200 ease-in"
               : "opacity-0 duration-100 ease-out",
             "absolute inset-0 flex h-full w-full items-center justify-center transition-opacity"
