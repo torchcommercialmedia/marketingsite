f381f3171fa5b88b1bbc098a978aecac98374c5c
Kelxety <llanzanarobinjohn@gmail.com>
2023-06-20 22:43:54 +0800

    commit build

--- api/dist/config/server.js
+++ api/dist/config/server.js
@@ -1,12 +1,13 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = ({ env }) => ({
-    host: env('HOST', '0.0.0.0'),
-    port: env.int('PORT', 1337),
+    host: env("HOST", "0.0.0.0"),
+    port: env.int("PORT", 1337),
     app: {
-        keys: env.array('APP_KEYS'),
+        keys: env.array("APP_KEYS"),
     },
+    url: "http://172.104.198.94",
     webhooks: {
-        populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+        populateRelations: env.bool("WEBHOOKS_POPULATE_RELATIONS", false),
     },
 });
