70e9ac139585cc74e94504b88672e429f64e44c2
Kelxety <llanzanarobinjohn@gmail.com>
2023-06-26 14:03:47 +0800

    added contact us

--- client/components/MainPage/ContactUs.tsx
+++ client/components/MainPage/ContactUs.tsx
@@ -1,79 +1,258 @@
 import React from "react";
 import { useForm, SubmitHandler } from "react-hook-form";
-import { AiOutlinePhone } from "react-icons/ai";
-import { MdEmail } from "react-icons/md";
-import { FaLocationArrow } from "react-icons/fa";
+import { useState } from "react";
+
+import { Switch } from "@headlessui/react";
+import { IoChevronDownCircleOutline } from "react-icons/io5";
 
 type Props = {};
 
 type Inputs = {
-  name: string;
+  firstName: string;
+  lastName: string;
   email: string;
-  subject: string;
-  message: string;
+  phoneNumber: string;
+  companyName: string;
+  companyType: string;
+  message?: string;
 };
 
 const ContactUs = (props: Props) => {
+  const [agreed, setAgreed] = useState(false);
+
+  function classNames(...classes: any[]) {
+    return classes.filter(Boolean).join(" ");
+  }
+
   const {
     register,
     handleSubmit,
-    watch,
     formState: { errors },
   } = useForm<Inputs>();
   const onSubmit: SubmitHandler<Inputs> = (formData) => {
-    window.location.href = `mailto:devjrl.programmer@gmail?subject=${formData.subject}&body=Hi, my name is ${formData.name}, ${formData.message} (${formData.email})`;
+    console.log(formData);
+    // window.location.href = `mailto:devjrl.programmer@gmail?subject=${formData.companyName}&body=Hi, my name is ${formData.firstName} ${formData.lastName}, ${formData.message} (${formData.email})`;
   };
   return (
     <section className="isolate bg-white px-6 py-24 sm:py-32 lg:px-8">
-      {/* <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
-        Request a Demo
-      </h3> */}
-
-      <div className="flex flex-col w-screen p-4 md:space-y-10">
-        <h4 className="text-lg md:text-2xl font-semibold text-center flex flex-row mx-auto">
-          <span className="underline decoration-red-500/50 ml-1">
-            Request a Demo.
-          </span>
-        </h4>
-        <form
-          onSubmit={handleSubmit(onSubmit)}
-          className="flex flex-col space-y-2 w-fit mx-auto"
+      <div className="isolate bg-white px-6 py-24 sm:py-32 lg:px-8">
+        <div
+          className="absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem]"
+          aria-hidden="true"
         >
-          <div className="md:space-x-2 md:flex w-full">
-            <input
-              required
-              {...register("name")}
-              placeholder="Name"
-              className="contactInput w-full mb-2 md:mb-0"
-              type="text"
-            />
-            <input
-              required
-              {...register("email")}
-              placeholder="Email"
-              className="contactInput w-full"
-              type="email"
-            />
+          <div
+            className="relative left-1/2 -z-10 aspect-[1155/678] w-[36.125rem] max-w-none -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-40rem)] sm:w-[72.1875rem]"
+            style={{
+              clipPath:
+                "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+            }}
+          />
+        </div>
+        <div className="mx-auto max-w-2xl text-left">
+          <h2 className="text-xl font-bold tracking-tight text-gray-900 sm:text-2xl">
+            Request a Demo
+          </h2>
+        </div>
+        <form
+          action="#"
+          method="POST"
+          className="mx-auto mt-10 max-w-2xl sm:mt-10"
+          onSubmit={handleSubmit(onSubmit)}
+        >
+          <div className="grid grid-cols-1 gap-x-4 gap-y-2 sm:grid-cols-2">
+            <div>
+              <label
+                htmlFor="firstName"
+                className="block text-sm font-semibold leading-6 text-gray-900"
+              >
+                First name
+              </label>
+              <div className="mt-2.5">
+                <input
+                  required
+                  {...register("firstName")}
+                  type="text"
+                  name="firstName"
+                  id="firstName"
+                  className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-red-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+            <div>
+              <label
+                htmlFor="lastName"
+                className="block text-sm font-semibold leading-6 text-gray-900"
+              >
+                Last name
+              </label>
+              <div className="mt-2.5">
+                <input
+                  {...register("lastName")}
+                  required
+                  type="text"
+                  name="lastName"
+                  id="lastName"
+                  className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-red-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+            <div>
+              <label
+                htmlFor="email"
+                className="block text-sm font-semibold leading-6 text-gray-900"
+              >
+                Email
+              </label>
+              <div className="mt-2.5">
+                <input
+                  required
+                  {...register("email")}
+                  type="email"
+                  name="email"
+                  id="email"
+                  autoComplete="email"
+                  className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-red-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+            <div>
+              <label
+                htmlFor="phoneNumber"
+                className="block text-sm font-semibold leading-6 text-gray-900"
+              >
+                Phone number
+              </label>
+              <div className="relative mt-2.5">
+                <div className="absolute inset-y-0 left-0 flex items-center">
+                  <label htmlFor="country" className="sr-only">
+                    Country
+                  </label>
+                  <select
+                    id="country"
+                    name="country"
+                    className="h-full rounded-md border-0 bg-transparent bg-none py-0 pl-2 pr-2 text-gray-400 focus:ring-0   focus:ring-inset focus:ring-red-600 sm:text-sm"
+                  >
+                    <option>US</option>
+                    <option>CA</option>
+                    <option>EU</option>
+                  </select>
+                </div>
+                <input
+                  {...register("phoneNumber")}
+                  required
+                  type="tel"
+                  name="phoneNumber"
+                  id="phoneNumber"
+                  className="block w-full rounded-md border-0 px-3.5 py-2 pl-16 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-red-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+            <div>
+              <label
+                htmlFor="companyName"
+                className="block text-sm font-semibold leading-6 text-gray-900"
+              >
+                Company Name
+              </label>
+              <div className="mt-2.5">
+                <input
+                  {...register("companyName")}
+                  required
+                  type="text"
+                  name="companyName"
+                  id="companyName"
+                  className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-red-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+            <div>
+              <label
+                htmlFor="phone-number"
+                className="block text-sm font-semibold leading-6 text-gray-900"
+              >
+                Company Type
+              </label>
+              <div className="relative mt-2.5">
+                <div className="block w-full rounded-md border-0 pr-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-red-600 sm:text-sm sm:leading-6">
+                  <label htmlFor="company-type" className="sr-only">
+                    Company Type
+                  </label>
+                  <select
+                    id="company-type"
+                    name="company-type"
+                    className="h-full rounded-md border-0 w-full bg-transparent py-2.5 bg-none pl-2 text-gray-900 outline-none sm:text-sm"
+                    required
+                  >
+                    <option value="" disabled hidden>
+                      Select Company Type
+                    </option>
+                    <option>Franchise Dealer</option>
+                    <option>Independent Dealer</option>
+                    <option>RV Dealer</option>
+                    <option>Repair Shop</option>
+                    <option>Tire Shop</option>
+                    <option>Photography Company</option>
+                    <option>Wrap Company</option>
+                    <option>Mobile Detail Company</option>
+                    <option>Others</option>
+                  </select>
+                </div>
+              </div>
+            </div>
+            <div className="sm:col-span-2">
+              <label
+                htmlFor="message"
+                className="block text-sm font-semibold leading-6 text-gray-900"
+              >
+                Message
+              </label>
+              <div className="mt-2.5">
+                <textarea
+                  name="message"
+                  id="message"
+                  rows={4}
+                  className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-red-600 sm:text-sm sm:leading-6"
+                  defaultValue={""}
+                />
+              </div>
+            </div>
+            <Switch.Group as="div" className="flex gap-x-4 sm:col-span-2">
+              <div className="flex h-6 items-center">
+                <Switch
+                  checked={agreed}
+                  onChange={setAgreed}
+                  className={classNames(
+                    agreed ? "bg-red-600" : "bg-gray-200",
+                    "flex w-8 flex-none cursor-pointer rounded-full p-px ring-1 ring-inset ring-gray-900/5 transition-colors duration-200 ease-in-out focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600"
+                  )}
+                >
+                  <span className="sr-only">Agree to policies</span>
+                  <span
+                    aria-hidden="true"
+                    className={classNames(
+                      agreed ? "translate-x-3.5" : "translate-x-0",
+                      "h-4 w-4 transform rounded-full bg-white shadow-sm ring-1 ring-gray-900/5 transition duration-200 ease-in-out"
+                    )}
+                  />
+                </Switch>
+              </div>
+              <Switch.Label className="text-sm leading-6 text-gray-600">
+                By selecting this, you agree to our{" "}
+                <a href="#" className="font-semibold text-red-600">
+                  privacy&nbsp;policy
+                </a>
+                .
+              </Switch.Label>
+            </Switch.Group>
           </div>
-          <input
-            required
-            {...register("subject")}
-            placeholder="Subject"
-            className="contactInput"
-            type="text"
-          />
-          <textarea
-            required
-            {...register("message")}
-            placeholder="Message"
-            className="contactInput"
-          />
-          <button
-            className="bg-[#F7AB0A] py-2 rounded-md text-black font-bold text-lg"
-            type="submit"
-          >
-            Submit
-          </button>
+          <div className="mt-10">
+            <button
+              type="submit"
+              className="block w-full rounded-md bg-red-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600"
+            >
+              Request a Demo
+            </button>
+          </div>
         </form>
       </div>
     </section>
