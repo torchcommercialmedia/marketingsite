99d5f96d6cb94ad0b10bf821810e69d5539ac28b
Kelxety <llanzanarobinjohn@gmail.com>
2023-06-27 00:35:47 +0800

    allow localhost

--- api/dist/config/middlewares.js
+++ api/dist/config/middlewares.js
@@ -1,14 +1,21 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = [
-    'strapi::errors',
-    'strapi::security',
-    'strapi::cors',
-    'strapi::poweredBy',
-    'strapi::logger',
-    'strapi::query',
-    'strapi::body',
-    'strapi::session',
-    'strapi::favicon',
-    'strapi::public',
+  "strapi::errors",
+  "strapi::security",
+  {
+    name: "strapi::cors",
+    config: {
+      enabled: true,
+      headers: "*",
+      origin: "*",
+    },
+  },
+  "strapi::poweredBy",
+  "strapi::logger",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
 ];
