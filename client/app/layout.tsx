6fda7d429f62a5cb211e0c5060a8e54daa3b83ef
Kelxety <llanzanarobinjohn@gmail.com>
2023-07-05 01:18:10 +0800

    added footer in layout

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import { Figtree } from "next/font/google";
 import Navbar from "@/components/Navbar";
+import Footer from "@/components/MainPage/Footer";
 
 const font = Figtree({ subsets: ["latin"] });
 
@@ -19,6 +20,7 @@ export default function RootLayout({
       <body className="bg-white/50">
         <Navbar />
         {children}
+        <Footer />
       </body>
     </html>
   );
