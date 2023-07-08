b399a42d3ce24251cf7d860a3af26e2cdec81bfc
Kelxety <llanzanarobinjohn@gmail.com>
2023-07-08 21:17:07 +0800

    added fetch data

--- client/app/faqs/page.tsx
+++ client/app/faqs/page.tsx
@@ -1,14 +1,18 @@
 import React from "react";
 import FAQs from "@/components/Faqs/FAQs";
+import { fetchDataFromApi } from "@/utils/fetch/fetchIndex";
+import { fetchFAQfromApi } from "@/utils/fetch/fetchFAQs";
 
 type Props = {};
 
-const page = (props: Props) => {
+const page = async (props: Props) => {
+  const params = "populate=deep";
+  const data = await fetchFAQfromApi("/faq-pages?", params);
   return (
     <div>
       <div className="mx-auto max-w-7xl px-6 py-24 sm:py-32 lg:px-8 lg:py-40">
         <div className="mx-auto max-w-4xl divide-y divide-black/50">
-          <FAQs />
+          <FAQs data={data} />
         </div>
       </div>
     </div>
