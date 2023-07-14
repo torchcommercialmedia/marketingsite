2c493c5a9a9a5bdd34c9795b465061de92e48846
Kelxety <llanzanarobinjohn@gmail.com>
2023-07-15 01:03:07 +0800

    remove min-h-screen

--- client/components/MainPage/DealersAndServiceProvider.tsx
+++ client/components/MainPage/DealersAndServiceProvider.tsx
@@ -12,7 +12,7 @@ type Props = {
 const DealersAndServiceProvider = ({ integration }: Props) => {
   console.log(integration.img?.data.attributes.url);
   return (
-    <section className="bg-white min-h-screen">
+    <section className="bg-white min-h-auto">
       <div className="relative isolate sm:mt-24">
         <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
           <div className="mx-auto flex max-w-2xl flex-col gap-16 px-6 py-16 ring-1 ring-white/10 sm:rounded-3xl sm:p-8 lg:mx-0 lg:max-w-none lg:flex-row lg:items-center lg:py-20 xl:gap-x-20 xl:px-20">
