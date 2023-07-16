1759dfee1ed1a7f5df3a96d4552cbf24d3c35659
Kelxety <llanzanarobinjohn@gmail.com>
2023-07-17 01:20:40 +0800

    added fetch images

--- client/components/MainPage/Integration.tsx
+++ client/components/MainPage/Integration.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { NativeIntegration } from "@/utils/types/types";
+import Image from "next/image";
 import { list } from "postcss";
 import React, { useState } from "react";
 
@@ -22,30 +23,26 @@ const Integration = ({ nativeIntegration }: Props) => {
       <p>Native Integrations with the tools you already use</p>
       <div className="sm:p-8 p-4"></div>
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
-        <div className="mx-auto grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-12 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 sm:gap-y-14 lg:mx-0 lg:max-w-none lg:grid-cols-5">
+        <div className="mx-auto grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-12 sm:max-w-xl sm:grid-cols-4 sm:gap-x-10 sm:gap-y-14 lg:mx-0 lg:max-w-none">
           {nativeIntegration.nativeIntContent.map((imge) => (
             <div
-              className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
+              className="col-span-1 max-h-12 w-full object-contain lg:col-span-1 flex justify-center items-center h-full"
               key={imge.id}
             >
-              {imge.id}
+              {imge.img.data && (
+                <Image
+                  src={
+                    process.env.NEXT_PUBLIC_WEBSITE_URL +
+                    imge.img?.data.attributes.url
+                  }
+                  width={imge.img?.data.attributes.width}
+                  height={400}
+                  alt={imge.img?.data.attributes.url!}
+                  className="object-contain my-auto h-full"
+                />
+              )}
             </div>
           ))}
-          <div className="col-span-2 max-h-12 w-full object-contain lg:col-span-1">
-            Image Here
-          </div>
-          <div className="col-span-2 max-h-12 w-full object-contain lg:col-span-1">
-            Image Here
-          </div>
-          <div className="col-span-2 max-h-12 w-full object-contain lg:col-span-1">
-            Image Here
-          </div>
-          <div className="col-span-2 max-h-12 w-full object-contain lg:col-span-1">
-            Image Here
-          </div>
-          <div className="col-span-2 max-h-12 w-full object-contain lg:col-span-1">
-            Image Here
-          </div>
         </div>
       </div>
     </div>
